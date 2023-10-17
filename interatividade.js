9268508a101a6c1308bc1c122dcbe8997d12754d
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-10-17 01:12:06 -0300

    script do relogio

---
+++ interatividade.js
@@ -0,0 +1,24 @@
+
+  const ponteiroSegundos = document.querySelector('.second-hand');
+  const ponteiroMinutos = document.querySelector('.min-hand');
+  const ponteiroHoras = document.querySelector('.hour-hand');
+
+  function setDate() {
+    const agora = new Date();
+
+    const segundos = agora.getSeconds();
+    const segundosGraus = ((segundos / 60) * 360) + 90;
+    ponteiroSegundos.style.transform = `rotate(${segundosGraus}deg)`;
+
+    const minutos = agora.getMinutes();
+    const minutosGraus = ((minutos / 60) * 360) + ((segundos/60)*6) + 90;
+    ponteiroMinutos.style.transform = `rotate(${minutosGraus}deg)`;
+
+    const horas = agora.getHours();
+    const horasGraus = ((horas / 12) * 360) + ((minutos/60)*30) + 90;
+    hourHand.style.transform = `rotate(${horasGraus}deg)`;
+  }
+
+  setInterval(setDate, 1000);
+
+  setDate();
